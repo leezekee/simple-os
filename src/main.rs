cf912911ddd441fb3d4ec8ca441156acd95a046f
leezekee <leezekee111156@qq.com>
2026-05-30 21:28:22 +0800

    chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// main.rs
+// src/main.rs
 
 #![no_std]
 #![no_main]
@@ -10,7 +10,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0x0f;
+        }
+    }
+
     loop {}
 }
